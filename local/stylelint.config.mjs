fa521fdb1deab38cfd11b656cc2b09c0b2ada265
Ваше Имя <you@example.com>
2025-10-24 18:36:03 +0300

    Refinement of the expanded TZ

--- local/stylelint.config.mjs
+++
@@ -1,7 +0,0 @@
-export default {
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
-  ignoreFiles: ['**/node_modules/**', '**/vendor/**', '**/bitrix/**'],
-  rules: {
-    'no-descending-specificity': null,
-  },
-};
